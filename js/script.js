271c5ca4ff35c38a692e58974db0511bb8d26eb8
KarolineMJ <karoline_mj@hotmail.com>
2018-11-19 12:04:28 +0100

    travelling photos

---
+++ js/script.js
@@ -0,0 +1,19 @@
+"use strict";
+//get the element from the document
+const myImg = document.querySelectorAll(".animate-me");
+
+//define the observer
+let observer = new IntersectionObserver(entries => {
+  //run through each entry
+  entries.forEach(entry => {
+    if (entry.intersectionRatio > 0) {
+      //add classList when the target area is in the screen
+      entry.target.classList.add("visible");
+    }
+  });
+});
+
+//run intersection observer for each image
+myImg.forEach(image => {
+  observer.observe(image);
+});
