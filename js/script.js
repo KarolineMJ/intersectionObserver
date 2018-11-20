c8da7b8e1943579577eba69b3777fd0ddca5eb84
Tiril Grimsrud <tiril_k_g@hotmail.com>
2018-11-20 13:12:49 +0100

    created template and cloned it

--- js/script.js
+++ js/script.js
@@ -1,6 +1,30 @@
 "use strict";
 //get the element from the document
 const myImg = document.querySelectorAll(".animate-me");
+const myTemp = document.querySelector("#travelTemp").content;
+let countries = [];
+
+window.addEventListener("DOMContentLoaded", fetchCountries);
+
+function fetchCountries() {
+  console.log("hi");
+
+  fetch("../countries.json")
+    .then(result => result.json())
+    .then(showCountries);
+}
+
+function showCountries(data) {
+  //console.log(data);
+  const countryName = Object.values(data);
+  console.log(countryName);
+  countryName.forEach(name => {
+    const clone = myTemp.cloneNode(true);
+    clone.querySelector("h2").textContent = name.slice();
+    clone.querySelector("img").src = "img/" + name.slice() + ".jpg";
+    document.querySelector("#travelContent").appendChild(clone);
+  });
+}
 
 //define the observer
 let observer = new IntersectionObserver(entries => {
@@ -19,3 +43,4 @@ let observer = new IntersectionObserver(entries => {
 myImg.forEach(image => {
   observer.observe(image);
 });
+*/
