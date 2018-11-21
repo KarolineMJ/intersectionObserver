95bfa5cfacdd39cb627f8c696be186243aaa5248
Tiril Grimsrud <tiril_k_g@hotmail.com>
2018-11-21 11:41:41 +0100

    made header animation work

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const myImg = document.querySelectorAll("img");
-
+const myHeader = document.querySelectorAll(".country");
 //run intersection observer for each image
 
 //define the observer
@@ -18,4 +18,19 @@ let observer = new IntersectionObserver(entries => {
 
 myImg.forEach(image => {
   observer.observe(image);
+});
+
+let headerObserver = new IntersectionObserver(entries => {
+  //run through each entry
+
+  entries.forEach(entry => {
+    if (entry.intersectionRatio > 0) {
+      //add classList when the target area is in the screen
+      entry.target.classList.add("appear");
+    }
+  });
+});
+
+myHeader.forEach(header => {
+  headerObserver.observe(header);
 });
