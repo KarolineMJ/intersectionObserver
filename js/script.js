ea80493f19dc69b4999ff4b61c9f17e29cbe5d3c
Tiril Grimsrud <tiril_k_g@hotmail.com>
2018-11-21 12:50:01 +0100

    added comments and finished

--- js/script.js
+++ js/script.js
@@ -1,14 +1,15 @@
 "use strict";
 
+//get the elements from the document
 const myImg = document.querySelectorAll("img");
 const myHeader = document.querySelectorAll(".country");
-//run intersection observer for each image
+const myContent = document.querySelector(".content");
 
 //define the observer
 let observer = new IntersectionObserver(entries => {
   //run through each entry
-
   entries.forEach(entry => {
+    //if the ratio is higher than zero, then animate target
     if (entry.intersectionRatio > 0) {
       //add classList when the target area is in the screen
       entry.target.classList.add("visible");
@@ -16,16 +17,15 @@ let observer = new IntersectionObserver(entries => {
   });
 });
 
+//observe the element, if the element is in view then run the previous function
 myImg.forEach(image => {
   observer.observe(image);
 });
 
+//repeat for each element
 let headerObserver = new IntersectionObserver(entries => {
-  //run through each entry
-
   entries.forEach(entry => {
     if (entry.intersectionRatio > 0) {
-      //add classList when the target area is in the screen
       entry.target.classList.add("appear");
     }
   });
@@ -34,3 +34,13 @@ let headerObserver = new IntersectionObserver(entries => {
 myHeader.forEach(header => {
   headerObserver.observe(header);
 });
+
+let contentObserver = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.intersectionRatio > 0) {
+      entry.target.classList.add("content-appear");
+    }
+  });
+});
+
+contentObserver.observe(myContent);
